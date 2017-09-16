2c82e391919ffdfb2c61245beff00db921068b47
Emilio Ortega <emiliojortega@lewisu.edu>
2017-09-16 18:14:16 -0500

    Update project1.cpp

--- Project2/project1.cpp
+++ Project2/project1.cpp
@@ -11,9 +11,7 @@ int main(){
 	string y = "2007";
 	int c = "red";
 	
-	Car::setColor(col) {
-		col = c;
-	}
+	Car::setColor(c);
 	//cout << "The model year is:" << carX.setYear << endl;
 	//cout << "The color of the car is:" << carX.setColor << endl;
 }
