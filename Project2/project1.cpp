e2c955def4751b1325f2e39983e828ea027ca15e
Emilio Ortega <emiliojortega@lewisu.edu>
2017-09-16 16:12:07 -0500

    main function

---
+++ Project2/project1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "project2.hxx"
+
+using namespace std;
+
+int main(){
+		
+	Car carX;
+	
+	cout << "The model year is:" << carX.setYear("2007"); << endl;
+	cout << "The color of the car is:" << carX.setColor("red"); << endl;
+}
