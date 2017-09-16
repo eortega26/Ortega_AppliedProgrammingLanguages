c19b8f5b08e9ae7186349f316b29a54e48d4849d
Emilio Ortega <emiliojortega@lewisu.edu>
2017-09-16 16:24:08 -0500

    Update project1.cpp

--- Project2/project1.cpp
+++ Project2/project1.cpp
@@ -7,6 +7,6 @@ int main(){
 		
 	Car carX;
 	
-	cout << "The model year is:" << carX.setYear("2007"); << endl;
-	cout << "The color of the car is:" << carX.setColor("red"); << endl;
+	cout << "The model year is:" << carX.setYear("2007") << endl;
+	cout << "The color of the car is:" << carX.setColor("red") << endl;
 }
