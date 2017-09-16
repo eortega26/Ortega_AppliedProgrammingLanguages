477fabf1d81fbf374007d35e140565415291d108
Emilio Ortega <emiliojortega@lewisu.edu>
2017-09-16 18:18:40 -0500

    Update project1.cpp

--- Project2/project1.cpp
+++ Project2/project1.cpp
@@ -9,7 +9,7 @@ int main(){
 	//carX.setColor();
 	//carX.setYear("2007");
 	string y = "2007";
-	char c = "red";
+	char* c = "red";
 	
 	carX.setColor(c);
 	//cout << "The model year is:" << carX.setYear << endl;
