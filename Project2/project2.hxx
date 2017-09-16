08dd5467b0954ccd120de88f2f674c432100e227
Emilio Ortega <emiliojortega@lewisu.edu>
2017-09-16 17:48:11 -0500

    Update project2.hxx

--- Project2/project2.hxx
+++ Project2/project2.hxx
@@ -1,15 +1,12 @@
-#ifndef PROJECT2_H
-#define PROJECT2_H
+#ifndef _PROJECT2_
+#define _PROJECT2_
 
 //Contains a class of a car
 
 class Car {
 	public:
-		std::string year;	// Year of the car.
-		std::string color;	// Color of the car.
-		
-		void setYear(std::string y);
-		void setColor(std::string col);
+		void setYear(std::string year);
+		void setColor(std::string color);
 };
 
 #endif
