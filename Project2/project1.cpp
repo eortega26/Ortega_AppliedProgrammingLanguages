5adafe35f93ae431e4dc8b9796fbd47b5634c7c5
Emilio Ortega <emiliojortega@lewisu.edu>
2017-09-16 18:24:29 -0500

    Update project1.cpp

--- Project2/project1.cpp
+++ Project2/project1.cpp
@@ -6,12 +6,11 @@ using namespace std;
 int main(){
 		
 	Car carX;
-	//carX.setColor();
-	//carX.setYear("2007");
 	string y = "2007";
 	string c = "red";
 	
 	carX.setColor(c);
+	carX.setYear(y);
 	//cout << "The model year is:" << carX.setYear << endl;
 	//cout << "The color of the car is:" << carX.setColor << endl;
 }
