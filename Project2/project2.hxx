63bcf6048f2262491e4eed400e108498d1bf0109
Emilio Ortega <emiliojortega@lewisu.edu>
2017-09-17 10:45:40 -0500

    Update project2.hxx

--- Project2/project2.hxx
+++ Project2/project2.hxx
@@ -1,19 +1,23 @@
 #ifndef _PROJECT2_
 #define _PROJECT2_
 
-//Contains a class of a car
+//Contains a class called Car
 
 class Car {
 	public:
-		std::string color;
-		std::string year;
 		
-	
-		int setYear(std::string y) {
-			return y;
+		std::string color;	// Variable for color of car.
+		std::string year;	// Variable for the year of the car.
+		
+		// Year member function
+		void setYear(std::string y) {
+			year = y;
+			std::cout << "The year of the car is: " << year << std::endl;
 		}
-		int setColor(std::string c) {
-			return c;
+		// Color member function
+		void setColor(std::string c) {
+			color = c;
+			std::cout << "The color of the car is: " << color << std::endl;
 		}
 };
 
